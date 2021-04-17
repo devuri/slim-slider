43885e6a64fde8d3ee8992fb0325dbe46a66a0a5
devuri <devuriko@gmail.com>
2021-04-17 08:37:58 -0500

    refactor: style is now slimslider

--- src/Slider.php
+++ src/Slider.php
@@ -76,7 +76,7 @@ final class Slider
 	 * @return void
 	 */
 	public function enqueue() {
-		wp_enqueue_style( 'slim-slider', Asset::uri() . '/css/style.css', array(), self::VERSION, 'all' );
+		wp_enqueue_style( 'slim-slider', Asset::uri() . '/css/slimslider.css', array(), self::VERSION, 'all' );
 		wp_enqueue_script( 'slim-slider-jssor', Asset::uri() . '/js/slim.jssor.slider.min.js', array( 'jquery' ), self::VERSION, true );
 		wp_enqueue_script( 'slim-slider', Asset::uri() . '/js/slimslider.min.js', array( 'slim-slider-jssor' ), self::VERSION, true );
 	}
