dd0e2f11c74b2d5061dcccc793586e430a7b0bae
devuri <devuriko@gmail.com>
2021-04-17 08:23:26 -0500

    feat: added additional options

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,4 +1,6 @@
 
+var slimsliderID = SlimSliderData;
+
 var _SlideshowTransitions = [
 	{
 		$Duration: 400,
@@ -6,21 +8,42 @@ var _SlideshowTransitions = [
 	}
 ];
 
+// @link https://www.jssor.com/development/api-options.html
 var slimslider_options = {
+	/**
+	 * $AutoPlay options
+	 * 0: no
+	 * 1: continuously
+	 * 2: stop at last slide
+	 * 4: stop on click
+	 * 8: stop on navigation,
+	 * 12: stop on click or user navigation
+	 */
 	$AutoPlay: 4,
-	$SlideDuration: 800,
-	$SlideHeight: 740,
+	$PauseOnHover: 1,
+	$SlideDuration: 800, // duration (in milliseconds)
+	$FillMode: 0, // 0: stretch 1: contain 2: cover 4: actual size 5: contain
+	$SlideHeight: slimsliderID.height,
 	$SlideEasing: $Jease$.$OutQuint,
 	$SlideshowOptions: {
 		$Class: $JssorSlideshowRunner$,
 		$Transitions: _SlideshowTransitions,
-		$TransitionsOrder: 1
+		$TransitionsOrder: 1 // 0: Random 1: In Order
 	},
 	$ArrowNavigatorOptions: {
-		$Class: $JssorArrowNavigator$
+		$Class: $JssorArrowNavigator$,
+		$ChanceToShow: 1, // 1: Mouse Over 2: Always
+		$Steps: 1        // Steps to go for each navigation request, default value is 1
 	},
 	$BulletNavigatorOptions: {
-		$Class: $JssorBulletNavigator$
+		$Class: $JssorBulletNavigator$,
+		$ChanceToShow: 2, // 0 Never, 1 Mouse Over, 2 Always
+		$ActionMode: 1,   // 0 None, 1 act by click, 2 act by mouse hover, 3 both, default value is 1
+		$Steps: 1,        // Steps to go for each navigation request, default value is 1
+		$Rows: 1,         // Specify lanes to arrange items, default value is 1
+		$SpacingX: 10,    // Horizontal space between each item in pixel, default value is 0
+		$SpacingY: 10,    // Vertical space between each item in pixel, default value is 0
+		$Orientation: 1   // The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
 	}
 };
 
@@ -43,3 +66,11 @@ slim_slider_init = function() {
     $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
 
 };
+
+/**
+ * The slider.
+ *
+ * Creates the slider output.
+ */
+var new_slimslider = new $JssorSlider$( "slim_slider_main_" + slimsliderID.id, slimslider_options );
+slim_slider_init();
