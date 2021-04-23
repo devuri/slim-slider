c3c6509e048fe9a5a27a46352710814a2a13678d
devuri <devuriko@gmail.com>
2021-04-22 20:14:38 -0500

    fix: Upgrading devuri/cpt-meta-box (0.1.5 => 0.1.7)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,24 +52,6 @@ class ComposerAutoloaderInit2cf1bd838b3c4ee620cc95822a13a3f8
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit2cf1bd838b3c4ee620cc95822a13a3f8::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire2cf1bd838b3c4ee620cc95822a13a3f8($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire2cf1bd838b3c4ee620cc95822a13a3f8($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
